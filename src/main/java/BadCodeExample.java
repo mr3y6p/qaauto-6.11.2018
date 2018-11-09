9d6608e8232b58077cb4b34e5702fb9d96e66f33
Mr3y6p <mr3y6p@gmail.com>
2018-11-09 22:07:04 +0200

    First commit. Lesson 2

---
+++ src/main/java/BadCodeExample.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BadCodeExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("https://google.com.ua/");
+        WebElement searchField = webDriver.findElement(By.name("q"));
+        searchField.sendKeys("Selenium");
+//        searchField.submit();
+        searchField.sendKeys(Keys.ENTER);
+
+        List<WebElement> resultList = webDriver.findElements(By.xpath("//div[@class='srg']/div[@class='g']"));
+        System.out.println(resultList.size());
+
+
+        webDriver.quit();
+    }
+
+}
