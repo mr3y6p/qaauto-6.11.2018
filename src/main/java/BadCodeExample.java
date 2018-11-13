1cd10a256ffcc6d736ae4f9f09c451c738b0dfbb
mykolazubrytskyi <mr3y6p@gmail.com>
2018-11-13 22:07:16 +0200

    Lesson #3

--- src/main/java/BadCodeExample.java
+++ src/main/java/BadCodeExample.java
@@ -15,18 +15,34 @@ public class BadCodeExample {
         WebElement searchField = webDriver.findElement(By.name("q"));
         String searchTerm = "Selenium";
         searchField.sendKeys(searchTerm);
-//        searchField.submit();
+        // searchField.submit(); is not user behaviour
         searchField.sendKeys(Keys.ENTER);
 
         List<WebElement> resultList = webDriver.findElements(By.xpath("//div[@class='srg']/div[@class='g']"));
         System.out.println(resultList.size());
-        String newLine = System.getProperty("line.separator");
+
+        // resolve by "for" loop
+
         for (int i=0; i<resultList.size(); i++){
             if (resultList.get(i).getText().contains(searchTerm)) {
-                System.out.println(i + ") " + resultList.get(i).getText() + newLine + searchTerm + " found" + newLine);
+                System.out.println(i + ") " + resultList.get(i).getText() + "\n" + searchTerm + " found\n");
             }
             else {
-                System.out.println(i + ") " + resultList.get(i).getText()+ newLine + searchTerm + " not found" + newLine);
+                System.out.println(i + ") " + resultList.get(i).getText() + "\n" + searchTerm + "not found\n");
+            }
+        }
+
+        // resolve by "for each"  loop
+        // for each WebElement 'result' in List of WebElements 'resultList' print Text.
+
+        for (WebElement result : resultList) {
+            String resultText = result.getText();
+
+            if (resultText.toLowerCase().contains(searchTerm.toLowerCase())) {
+                System.out.println("searchTerm " + searchTerm + " found in block:\n" + resultText);
+            }
+            else {
+                System.out.println("searchTerm " + searchTerm + "not found in block:\n" + resultText);
             }
         }
 
